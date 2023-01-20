05a9807c1952cb2eb6432614893820d16a8f53bb
José Vitor do Nascimento <jose.nascimento@mg.senac.br>
2023-01-20 11:41:52 -0300

    Estrutura-de-repetição-0067

---
+++ acumulador.py
@@ -0,0 +1,17 @@
+# acumulador com while
+total = 0
+itens = 0
+preco = 1
+print ("Digite o preço dos produtos - ou 0 para encerrar.")
+while preco !=0:
+    preco = float(input("Preço? "))
+    if preco!=0:
+        total += preco
+        itens += 1
+print ("-" * 35)
+print ("Quantidade de itens:",itens)
+print ("Valor total da compra:",total)
+print ("-" * 35)   
+        
+        
+        
